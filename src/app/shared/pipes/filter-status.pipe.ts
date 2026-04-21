a6a2c379fcca5df5ad2e5e6b14d39c61f8311171
DhaferSouid <dhafer-souid@caregiver-team.fr>
2026-04-21 13:15:52 +0100

    front after deployment

--- src/app/shared/pipes/filter-status.pipe.ts
+++ src/app/shared/pipes/filter-status.pipe.ts
@@ -3,6 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'filterStatus' })
 export class FilterStatusPipe implements PipeTransform {
   transform(items: any[], status: string): number {
+
+
     return items.filter(i => i.status === status).length;
+
+
+    
   }
 }
